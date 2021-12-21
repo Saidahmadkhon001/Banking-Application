537408f4fbdae6542972fe58530a2ee7a548e473
unknown <saidahmadkhon001@gmail.com>
2021-12-21 15:46:03 +0100

    First Commi

---
+++ Bank.java
@@ -0,0 +1,9 @@
+public class Bank {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Account person = new Account("Person Name", "A00001");
+        person.showMenu();
+
+    }
+}
